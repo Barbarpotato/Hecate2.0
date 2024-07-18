b90b474730eb4c274d8198d86b5a086082c8134c
Barbarpotato <darmawanjr88@gmail.com>
2024-07-18 22:07:25 +0800

    remove strict auth from get api endpoint

--- src/pages/api/typewriter/index.js
+++ src/pages/api/typewriter/index.js
@@ -8,41 +8,49 @@ const withMiddleware = (handler) => async (req, res) => {
     return handler(req, res);
 };
 
+const postHandler = async (req, res) => {
+    const { body } = req;
+
+    // Validate the data coming in
+    if (!body.title) {
+        return res.status(400).json({ message: 'Title is required' });
+    }
+
+    try {
+        // Insert data into db
+        const post = await prisma.typewriter.create({
+            data: {
+                title: body.title,
+                created: new Date(),
+            },
+        });
+        return res.status(201).json({ message: 'Successfully created Typewriter Data', post });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Error creating post' });
+    }
+};
+
+const getHandler = async (req, res) => {
+    try {
+        // Get all data from db
+        const posts = await prisma.typewriter.findMany();
+        return res.status(200).json(posts);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Error retrieving posts' });
+    }
+};
+
 const handler = async (req, res) => {
     if (req.method === 'POST') {
-        const { body } = req;
-
-        // Validate the data coming in
-        if (!body.title) {
-            return res.status(400).json({ message: 'Title is required' });
-        }
-
-        try {
-            // Insert data into db
-            const post = await prisma.typewriter.create({
-                data: {
-                    title: body.title,
-                    created: new Date(),
-                },
-            });
-            return res.status(201).json({ message: 'Successfully created Typewriter Data', post });
-        } catch (error) {
-            console.error(error);
-            return res.status(500).json({ error: 'Error creating post' });
-        }
+        return withMiddleware(postHandler)(req, res);
     } else if (req.method === 'GET') {
-        try {
-            // Get all data from db
-            const posts = await prisma.typewriter.findMany();
-            return res.status(200).json(posts);
-        } catch (error) {
-            console.error(error);
-            return res.status(500).json({ error: 'Error retrieving posts' });
-        }
+        return getHandler(req, res);
     } else {
         res.setHeader('Allow', ['POST', 'GET']);
         return res.status(405).end(`Method ${req.method} Not Allowed`);
     }
 };
 
-export default withMiddleware(handler);
+export default handler;
